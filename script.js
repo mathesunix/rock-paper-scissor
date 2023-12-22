a388b2c34fa269b6bc5acee86882ff1b9167c8fe
Matheus <mag08081996@gmail.com>
2023-12-22 17:33:23 -0300

    Remove console.log from inside round function
    We're not going to use console.log in the future, so I removed it

--- script.js
+++ script.js
@@ -17,37 +17,37 @@ function getComputerChoice() {
 function round(playerSelection, computerSelection) {
     if (computerSelection == 'rock') {
         if (playerSelection == 'scissor') {
-            console.log('You lose! Rock beats scissor');
+            return 'You lose! Rock beats scissor'
         }
         else if (playerSelection == 'paper') {
-            console.log('You win! Paper beats rock');
+            return 'You win! Paper beats rock'
         }
         else {
-            console.log("It's a tie!");
+            return "It's a tie!"
         }
     }
     else if (computerSelection == 'paper') {
         if (playerSelection == 'scissor') {
-            console.log('You win! Scissor beats paper');
+            return 'You win! Scissor beats paper'
         }
         else if (playerSelection == 'paper') {
-            console.log("It's a tie!");
+            return "It's a tie!"
         }
         else {
-            console.log('You lose! Paper beats rock');
+            return 'You lose! Paper beats rock'
         }
     }
     else {
         if (playerSelection == 'scissor') {
-            console.log("It's a tie!");
+            return "It's a tie!"
         }
         else if (playerSelection == 'paper') {
-            console.log('You lose! Scissor beats paper');
+            return 'You lose! Scissor beats paper'
         }
         else {
-            console.log('You win! Rock beats scissor');
+            return 'You win! Rock beats scissor'
         }
     }
 }
 
-round(playerSelection, getComputerChoice());
+console.log(round(playerSelection.toLowerCase(), getComputerChoice()));
