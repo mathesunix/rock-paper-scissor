cc7664bff057bd1705e86a8b52ee0a0b00352167
Matheus <mag08081996@gmail.com>
2023-12-21 22:32:03 -0300

    Code function that emulates the computer's choice

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+    let numberSymbol = Math.random();
+
+    if (numberSymbol <= 0.33) {
+        return 'rock';
+    }
+    else if (numberSymbol <= 0.66) {
+        return 'paper';
+    }
+    else {
+        return 'scissor';
+    }
+}
+
+let symbol = getComputerChoice();
+console.log(symbol);
