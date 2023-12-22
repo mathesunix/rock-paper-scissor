38fd426d2bee44b8f606ab429b29f4623a1387b1
Matheus <mag08081996@gmail.com>
2023-12-22 17:20:43 -0300

    Add function that emulates a round

--- script.js
+++ script.js
@@ -23,7 +23,31 @@ function round(playerSelection, computerSelection) {
             console.log('You win! Paper beats rock');
         }
         else {
-            console.log('It"s a tie!');
+            console.log("It's a tie!");
+        }
+    }
+    else if (computerSelection == 'paper') {
+        if (playerSelection == 'scissor') {
+            console.log('You win! Scissor beats paper');
+        }
+        else if (playerSelection == 'paper') {
+            console.log("It's a tie!");
+        }
+        else {
+            console.log('You lose! Paper beats rock');
+        }
+    }
+    else {
+        if (playerSelection == 'scissor') {
+            console.log("It's a tie!");
+        }
+        else if (playerSelection == 'paper') {
+            console.log('You lose! Scissor beats paper');
+        }
+        else {
+            console.log('You win! Rock beats scissor');
         }
     }
 }
+
+round(playerSelection, getComputerChoice());
